debfc7830806508f11d77afe3998065dd7bb3f40
Nomi Vos <nvoswork@gmail.com>
2016-07-04 16:34:24 -0500

    fizzbuzz #4

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,16 +1,16 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when a divisor of 3 is passed in' do
+  it 'returns "fizz" when a number divisible by 3 is passed in' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-  it 'returns "buzz" when a divisor of 5 is passed in' do
+  it 'returns the number back when a number not divisible by 3 or 5 is passed in' do
+    expect(fizzbuzz(2)).to eq 2
+  end
+  it 'returns "buzz" when a number divisible by 5 is passed in' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-  it 'returns "fizzbuzz" when a divisor of 15 is passed in' do
+  it 'returns "fizzbuzz" when a number divisible by 15 is passed in' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
-  end
-  it 'doesn\'t return "fizz" when not a divisor of 3' do
-    expect(fizzbuzz(1)).to eq 1
   end
 end
